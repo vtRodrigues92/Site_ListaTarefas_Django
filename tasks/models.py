b0016d122c9390875f52ebac230a8a441631326e
Victor de Oliveira Rodrigues <victor.rodrigues@dasa.com.br>
2022-08-06 00:27:28 -0300

    listaTarefa

---
+++ tasks/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Task(models.Model):
+
+    STATUS = (
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+        ('todo', 'Todo'),
+    )
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    responsavel = models.CharField(max_length=255)
+    
+    done = models.CharField(
+        max_length=5,
+        choices=STATUS,
+    )
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+       return self.title
